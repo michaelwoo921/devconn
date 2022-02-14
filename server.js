14f5f8e4357606d2356d0d84e13074ecc18b1aaa
Jung Woo <michaelwoo921@gmail.com>
2022-02-13 17:28:37 -0800

    register user return token

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 // connect database
 connectDB();
 
+// init midleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // define routes
