5b6abfb4e51699dbc29ab063af4043d564e499b6
Jung Woo <michaelwoo921@gmail.com>
2022-02-17 17:48:43 -0800

    delete post and add post

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -23,3 +23,7 @@ export const ACCOUNT_DELETED = 'DELETE_ACCOUNT';
 
 export const GET_POSTS = 'GET_POST';
 export const POST_ERROR = 'POST_ERROR';
+
+export const UPDATE_LIKES = 'UPDATE_LIKES';
+export const DELETE_POST = 'DELETE_POST';
+export const ADD_POST = 'ADD_POST';
